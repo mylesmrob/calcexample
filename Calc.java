fdca842e6f38779fc9c32e94b0f5f83abb9e4b33
mylesmrob <mylesmrobinson@gmail.com>
2021-03-18 16:44:00 -0400

    calc example

---
+++ Calc.java
@@ -0,0 +1,23 @@
+package marcheighteenthclass;
+
+public class Calc {
+
+    public Calc(){
+    }
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int sub(int a, int b){
+        return a-b;
+    }
+
+    public int mult(int a,int b){
+        return a*b;
+    }
+
+    public int div(int a,int b){
+        return a/b;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+package marcheighteenthclass;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calc calculator = new Calc();
+        System.out.println(calculator.add(8,7));
+        System.out.println(calculator.sub(10,5));
+        System.out.println(calculator.mult(8,5));
+        System.out.println(calculator.div(20,5));
+    }
+}
